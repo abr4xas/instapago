54642a0a0fc7e4f0cfce2f77f059710ffccf1bb2
abr4xas <bullgram@gmail.com>
2017-01-28 21:29:31 +0000

    Apply fixes from StyleCI

--- includes/admin-options.php
+++ includes/admin-options.php
@@ -2,7 +2,7 @@
     <?php _e($this->medthod_title); ?>
 </h3>
 <p>
-    <?php _e($this->method_description ); ?>
+    <?php _e($this->method_description); ?>
 </p>
 <table class="form-table">
     <?php $this->generate_settings_html(); ?>
